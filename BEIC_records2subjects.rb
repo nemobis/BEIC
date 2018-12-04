800306d93233c063e9d0fd7d5e6c77629a9d2d3d
Federico Leva <federicoleva@tiscali.it>
2018-12-04 10:07:31 +0200

    Support HTTPS in OPAC SBN export

--- BEIC_records2subjects.rb
+++ BEIC_records2subjects.rb
@@ -18,11 +18,11 @@ subjects.write("Identificativo\tCampo\tSottocampi\n")
 File.readlines(ARGV[0]).each do |bid|
     puts bid
     bid = bid.strip
-    url = URI.parse('http://opac.sbn.it/opacsbn/opaclib?db=solr_iccu&select_db=solr_iccu&nentries=1&from=1&searchForm=opac/iccu/error.jsp&resultForward=opac/iccu/scarico_uni.jsp&do_cmd=search_show_cmd&format=unimarc&rpnlabel=+Identificativo+SBN+%3D+' + bid  + '+%28parole+in+AND%29+&rpnquery=%40attrset+bib-1++%40attr+1%3D1032+%40attr+4%3D6+%22' + bid  + '%22&totalResult=1&fname=none')
+    url = URI.parse('https://opac.sbn.it/opacsbn/opaclib?db=solr_iccu&select_db=solr_iccu&nentries=1&from=1&searchForm=opac/iccu/error.jsp&resultForward=opac/iccu/scarico_uni.jsp&do_cmd=search_show_cmd&format=unimarc&rpnlabel=+Identificativo+SBN+%3D+' + bid  + '+%28parole+in+AND%29+&rpnquery=%40attrset+bib-1++%40attr+1%3D1032+%40attr+4%3D6+%22' + bid  + '%22&totalResult=1&fname=none')
     req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) { |http|
-        http.request(req)
-                                              }
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = (url.scheme == "https")
+    res = http.request(req)
     reader = MARC::ForgivingReader.new(StringIO.new(res.body))
     for record in reader
         for subject in record.find_all {|field| ('606'..'676') === field.tag}
